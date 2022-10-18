c1db7daabe03fdf3d50c5bc1b8c397388d70d9a2
Jin Hwee <jinhwee98@gmail.com>
2022-10-18 17:12:48 +0800

    Defined Graph and Node classes; added main script

---
+++ classesDefinition.py
@@ -0,0 +1,48 @@
+class Graph:
+    def __init__(self, vertices, adjacencyDictionary):
+        # @params vertices: dictionary in the format int(id): Node(int(i), data)
+        # @params adjacencyDictionary: adjacencyDictionary[id] is a dictionary that 
+        #                              represents id of direct neighbours and len(adjacencyDictionary) <= n; 
+        #                              where n = number of vertices in graph
+        self.vertices = vertices 
+        self.adjDictionary = adjacencyDictionary
+    
+    def add_vertex(self, newVertexID, data, listOfVertexNeighbours):
+        # @params newVertexID: Node object for vertex to be added to graph topology
+        # @params data: data which the newVertex will have
+        # @params listOfVertexNeighbours: list of neighbour ids newVertex is connected to
+        self.vertices[newVertexID] = Node(newVertexID, data)
+        self.adjDictionary[newVertexID] = listOfVertexNeighbours
+        self.vertices[newVertexID].update_neighbours(listOfVertexNeighbours)
+        for id in listOfVertexNeighbours:
+            self.vertices[id].update_neighbours(newVertexID)
+
+    # to print out information on the graph
+    def print_information(self):
+        print(f"All vertices currently present in graph network: {list(self.vertices.keys())}")
+        for id, _ in self.vertices.items():
+            self.vertices[id].print_information()
+
+class Node:
+    def __init__(self, id, data):
+        # @params id: unique vertex identification number
+        # @params data: data that this particular vertex has (training data)
+        # @params listOfDirectNeighbours: list containing ids of vertices it is directly connected to; 
+        #                                 listOfDirectNeighbours is empty at start, and will be populated
+        self.id = id
+        self.data = data 
+        self.neighbours = []
+        self.initialized = False
+
+    def update_neighbours(self, vertexID):
+        # @params vertexID: unique identification of new vertex
+        # @params initialized: whether the node has been init
+        if not self.initialized:
+            self.neighbours.extend(vertexID)
+            self.initialized = True
+        else:
+            self.neighbours.append(vertexID)
+    
+    # to print out information on the node
+    def print_information(self):
+        print(f"Node {self.id} has data {self.data} and is connected to nodes {self.neighbours}")
---
+++ main.py
@@ -0,0 +1,36 @@
+from classesDefinition import Graph, Node, Edge
+
+def generate_graph():
+
+    # generate vertices and store in nodeDictionary, in the format int(id): Node(i, data)
+    nodeDictionary = {}
+    for i in range(1, 8):
+        tmp = Node(i, None)
+        nodeDictionary[i] = tmp
+
+    # generate adjacencyDictionary, in the format int(id): [int(id of neighbour), ...]
+    neighbourDictionary = {}
+    neighbourDictionary[nodeDictionary[1].id] = [nodeDictionary[2].id, nodeDictionary[3].id]
+    neighbourDictionary[nodeDictionary[2].id] = [nodeDictionary[1].id, nodeDictionary[3].id]
+    neighbourDictionary[nodeDictionary[3].id] = [nodeDictionary[1].id, nodeDictionary[2].id, nodeDictionary[4].id, nodeDictionary[6].id, nodeDictionary[7].id]
+    neighbourDictionary[nodeDictionary[4].id] = [nodeDictionary[3].id, nodeDictionary[5].id]
+    neighbourDictionary[nodeDictionary[5].id] = [nodeDictionary[4].id, nodeDictionary[6].id]
+    neighbourDictionary[nodeDictionary[6].id] = [nodeDictionary[3].id, nodeDictionary[5].id, nodeDictionary[7].id]
+    neighbourDictionary[nodeDictionary[7].id] = [nodeDictionary[3].id, nodeDictionary[6].id]
+
+    # to update node of its neighbours after initialization
+    for id in nodeDictionary.keys():
+        nodeDictionary[id].update_neighbours(neighbourDictionary[id])
+    
+    # creating the first graph
+    print("Testing the creation of the graph instance...")
+    graphTest = Graph(nodeDictionary, neighbourDictionary)
+    graphTest.print_information()
+
+    # adding a sample vertex
+    print("\nAdding an additional vertex...")
+    graphTest.add_vertex(10, None, [1, 2])
+    graphTest.print_information()
+
+if __name__ == "__main__":
+    generate_graph()
