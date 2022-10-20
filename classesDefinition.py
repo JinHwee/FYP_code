af6b9501a5476ebd8c43f5ac04f3ae42530ae49d
Jin Hwee <jinhwee98@gmail.com>
2022-10-21 00:08:08 +0800

    modifications to classes defined, main test function

--- classesDefinition.py
+++ classesDefinition.py
@@ -1,14 +1,20 @@
-import random
+import random, math
+
+random.seed(42)     # setting a fixed seed for experiments
 
 class Graph:
-    def __init__(self, vertices, adjacencyDictionary):
+    # initialize one class variable for edgeWeighsDictionary, so that one can update the weights of edges from Node class
+    # is there a better implementation of this? How to update weights of edges using Graph instance from Node instance?
+    edgeWeighsDictionary = None
+
+    def __init__(self, vertices, adjacencyDictionary, edgeWeightsDictionary):
         # @params vertices: dictionary in the format int(id): Node(int(i), data)
         # @params adjacencyDictionary: adjacencyDictionary[id] is a dictionary that 
         #                              represents id of direct neighbours and len(adjacencyDictionary) <= n; 
         #                              where n = number of vertices in graph
-        self.seed = random.seed(42)     # setting a fixed seed for experiments
         self.vertices = vertices 
         self.adjDictionary = adjacencyDictionary
+        Graph.edgeWeighsDictionary = edgeWeightsDictionary
     
     def add_vertex(self, newVertexID, data, listOfVertexNeighbours):
         # @params newVertexID: Node object for vertex to be added to graph topology
@@ -17,14 +23,38 @@ class Graph:
         self.vertices[newVertexID] = Node(newVertexID, data)
         self.adjDictionary[newVertexID] = listOfVertexNeighbours
         self.vertices[newVertexID].update_neighbours(listOfVertexNeighbours)
+        self.__update_edges_weight(newVertexID, listOfVertexNeighbours)             # adds (newVertexID, neighbourID) to edgeWeighsDictionary
         for id in listOfVertexNeighbours:
             self.vertices[id].update_neighbours(newVertexID)
 
-    # to print out information on the graph
+    # not supposed to be called externally (private update function)
+    def __update_edges_weight(self, vertexID, listOfNeighbours):
+        for id in listOfNeighbours:
+            # assumes that the weight of the edges are 1s
+            # self.edgeWeighsDictionary[(vertexID, id)] = 1
+            # assume that the weight of src->dest =/= dest->src   
+            self.edgeWeighsDictionary[(vertexID, id)] = random.randint(50, 100) 
+    
+    # check if edge exist; assuming that it might not be a bidirectional edge
+    def check_edge(cls, srcID, destID):
+        if (srcID, destID) in cls.edgeWeighsDictionary.keys():
+            return cls.edgeWeighsDictionary[(srcID, destID)]
+        elif (destID, srcID) in cls.edgeWeighsDictionary.keys():
+            return cls.edgeWeighsDictionary[(destID, srcID)]
+        return float(math.inf)
+
+    # to print out adjacency matrix of graph
     def print_information(self):
-        print(f"All vertices currently present in graph network: {list(self.vertices.keys())}")
-        for id, _ in self.vertices.items():
-            self.vertices[id].print_information()
+        print('\t', end='')
+        for id in self.vertices.keys():
+            print(id, end='\t')
+        print()
+        for id in self.vertices.keys():
+            print(id, end='\t')
+            for neighbourID in self.vertices.keys():
+                results = self.check_edge(id, neighbourID)
+                print(results, end="\t") if math.isinf(results) == False else print("inf", end='\t')
+            print()
 
 class Node:
     def __init__(self, id, data):
@@ -44,8 +74,13 @@ class Node:
         # @params initialized: whether the node has been init
         if not self.initialized:
             self.neighbours.extend(vertexID)
-            self.initialized = True
+            self.initialized = True         # if just initialized, assumes edgeWeighsDictionary had been populated prior to calling this function
         else:
+            # adds (currentID, newVertexID) to edgeWeighsDictionary
+            # assumes that the weight of the edge is 1, update the class variable directly
+            # Graph.edgeWeighsDictionary[(self.id, vertexID)] = 1    
+            # assume that the weight of src->dest =/= dest->src   
+            Graph.edgeWeighsDictionary[(self.id, vertexID)] = random.randint(1, 100) 
             self.neighbours.append(vertexID)
     
     # to print out information on the node
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from classesDefinition import Graph, Node, Edge
+import random
+from classesDefinition import Graph, Node
 
 def generate_graph():
 
@@ -18,19 +19,33 @@ def generate_graph():
     neighbourDictionary[nodeDictionary[6].id] = [nodeDictionary[3].id, nodeDictionary[5].id, nodeDictionary[7].id]
     neighbourDictionary[nodeDictionary[7].id] = [nodeDictionary[3].id, nodeDictionary[6].id]
 
+    edgeDictionary = {}     # weights of each edges
     # to update node of its neighbours after initialization
     for id in nodeDictionary.keys():
+        for neighbourID in neighbourDictionary[id]:
+            # assume that the weight of the edge is 1
+            # edgeDictionary[(id, neighbourID)] = 1
+            # assume that the weight of src->dest =/= dest->src                  
+            edgeDictionary[(id, neighbourID)] = random.randint(0, 100)
         nodeDictionary[id].update_neighbours(neighbourDictionary[id])
     
     # creating the first graph
     print("Testing the creation of the graph instance...")
-    graphTest = Graph(nodeDictionary, neighbourDictionary)
+    graphTest = Graph(nodeDictionary, neighbourDictionary, edgeDictionary)
     graphTest.print_information()
 
     # adding a sample vertex
     print("\nAdding an additional vertex...")
     graphTest.add_vertex(10, None, [1, 2])
     graphTest.print_information()
+
+    print()
+    for id, nodeInstance in nodeDictionary.items():
+        nodeInstance.print_information()
+
+    print()
+    for IDpair, weight in Graph.edgeWeighsDictionary.items():
+        print(IDpair, weight)
 
 if __name__ == "__main__":
     generate_graph()
